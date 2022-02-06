cc35b15592af3801ca3208631899b0e673335843
danielmarius1995 <danieladam01995@gmail.com>
2022-02-06 17:06:08 +0200

    New commit

--- express-with-ts/src/app.ts
+++ express-with-ts/src/app.ts
@@ -13,23 +13,12 @@ const app: Application = express();
 app.use(express.json());
 app.use(helmet());
 
-const TEAMS = [
-  { id: 1, name: 'Real Madrid', league: 'La Liga' },
-  { id: 2, name: 'Barcelona', league: 'La Liga' },
-  { id: 3, name: 'Manchester United', league: 'Premier League' },
-  { id: 4, name: 'Liverpool', league: 'Premier League' },
-  { id: 5, name: 'Arsenal', league: 'Premier League' },
-  { id: 6, name: 'Inter', league: 'Serie A' },
-  { id: 7, name: 'Milan', league: 'Serie A' },
-  { id: 8, name: 'Juventus', league: 'Serie A' },
-];
-
 app.get('/:id', (req: Request, res: Response) => {
   if (req.params.id.includes('id')) {
     throw new BadRequestError('forgot something?', HttpStatusMessage.BAD_REQUEST);
   }
 
-  res.status(200).send(TEAMS);
+  res.status(200).send(HttpStatusMessage.OK);
 });
 
 app.all('*', (req: Request, res: Response, next: NextFunction) => {
