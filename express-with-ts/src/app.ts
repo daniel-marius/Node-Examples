dc4821097f67df033d0016504d663a061d66fa87
danielmarius1995 <danieladam01995@gmail.com>
2022-02-14 21:05:26 +0200

    New commit

--- express-with-ts/src/app.ts
+++ express-with-ts/src/app.ts
@@ -1,6 +1,7 @@
 import express, { Application, NextFunction, Request, Response } from 'express';
 import helmet from 'helmet';
-
+import 'express-async-errors';
+const fetch = (...args: any[]) => import('node-fetch').then(({default: fetch}) => fetch(...args));
 import { BadRequestError } from './errors/bad-request-error';
 import { NotFoundError } from './errors/not-found-error';
 import { HttpStatusMessage } from './errors/http-status';
@@ -13,16 +14,23 @@ const app: Application = express();
 app.use(express.json());
 app.use(helmet());
 
-app.get('/:id', (req: Request, res: Response) => {
-  if (req.params.id.includes('id')) {
-    throw new BadRequestError('forgot something?', HttpStatusMessage.BAD_REQUEST);
+app.get('/', async (req: Request, res: Response) => {
+  // if (req.params.id.includes('id')) {
+  //   throw new BadRequestError('forgot something?', HttpStatusMessage.BAD_REQUEST);
+  // }
+
+  const response = await fetch('https://fakestoreapi.com/products');
+  const json = await response.json();
+
+  if (json.length <= 20) {
+    throw new BadRequestError('forgot something?', 'bad request error!');
   }
 
-  res.status(200).send(HttpStatusMessage.OK);
+  res.status(200).send(json);
 });
 
-app.all('*', (req: Request, res: Response, next: NextFunction) => {
-  throw new NotFoundError(`URL: ${req.originalUrl} Not Found!`, HttpStatusMessage.NOT_FOUND);
+app.all('*', async (req: Request, res: Response, next: NextFunction) => {
+  throw new NotFoundError('Please try again!', `Route: ${req.originalUrl} Not Found!`, );
 });
 
 app.use(errorHandler);
